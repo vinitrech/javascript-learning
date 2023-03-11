526847901c80eb87bff8d6cd844eea9cc2339f1d
Vinicius <vinitrech@gmail.com>
2023-03-11 13:53:44 -0300

    adding operators

--- variables_and_operators/operators.js
+++ variables_and_operators/operators.js
@@ -16,3 +16,12 @@ console.log(students === 20 && 1 === 1); // and
 console.log(students === 20 || 1 === 1); // or
 console.log(students != 20); // not equal
 console.log(students !== 20); // not identical
+
+let ob = {
+    a: 1,
+    b: 2
+}
+
+ob.a ||= 2; // set to 3 if value does not exist, if exists, keep current value
+ob.a &&= 2; // set to 3 if value is already set
+ob.a ??= 2; // set to 2 if value is null or undefined
